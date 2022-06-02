77cbebbc67bbdb6a130f5e71a9581433fa57ccb7
Salome <salome_rosa@hotmail.com>
2022-06-01 20:36:30 -0500

    cambios

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,5 +22,11 @@ module "rds-potgres" {
   source = "./modules/rds-pg"
   subnets = [module.networking.subnet_data1_id, module.networking.subnet_data2_id]
   vpc_id = module.networking.vpc_id
-  wordpress_rdb_sg_id = module.networking.wordpress_rdb_sg_i
+  wordpress_rdb_sg_id = module.networking.wordpress_rdb_sg_id
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+  subnets = [module.networking.subnet1_id, module.networking.subnet2_id]
+  wordpress_sg_id = module.networking.wordpress_sg_id
 }
